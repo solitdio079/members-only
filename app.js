272de77993c0ffcfadfe84df7a9d70cd3f5293f2
Djoko Keita <djokokeita@Djokos-MacBook-Air-2.local>
2026-04-22 09:07:57 +0300

    Signup page

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require("dotenv").config()
 
 const app = express()
 
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,"public")))
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname,"views"))
 
